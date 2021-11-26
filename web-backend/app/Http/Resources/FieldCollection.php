49916ca64c410a19c084b8c1b78763e8bf7d13bc
Toms <toms.polaks@applyit.lv>
2021-11-26 13:25:29 +0200

    add policy

--- web-backend/app/Http/Resources/FieldCollection.php
+++ web-backend/app/Http/Resources/FieldCollection.php
@@ -3,6 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use App\Http\Resources\FieldResource;
+use App\Models\Fields;
+use Illuminate\Support\Facades\Gate;
 
 class FieldCollection extends ResourceCollection
 {
@@ -12,14 +15,19 @@ class FieldCollection extends ResourceCollection
      * @param  \Illuminate\Http\Request  $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
-
+    // public static $wrap = 'field';
     public $collects = FieldResource::class;
     
     public function toArray($request)
     {
-#        return parent::toArray($request);    
+#        return parent::toArray($request);
+        $user = $request->user();
         return [
-            'data' => $this->collection
+            'data' => $this->collection,
+            'actions' => [
+                // 'create' => $request->user()->can('create', Fields::class)
+                // 'create' => $user->can('create', $this->resource),
+            ]
         ];
     }
 }
